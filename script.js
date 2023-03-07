fbfedc3788d6c97aff54f5a903daad4417720386
Gabigks <gabrielgks10@gmail.com>
2023-03-06 21:54:26 -0300

    variables and comments

---
+++ script.js
@@ -0,0 +1,12 @@
+//comentario linha
+/* comentario bloco*/
+
+//Conceitos basicos
+/*var nome = "Gabriel";
+
+var idade = prompt("Qual sua idade");
+
+document.write("<br></br>");
+document.write("<h2>teste</h2>");
+document.write("<img src='http://google.com/google.jpg' />");
+*/
